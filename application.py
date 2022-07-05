a60793265b951f0bc68bad967749c3aa1eb06f31
Ono <81604558+Onochieu@users.noreply.github.com>
2022-07-05 05:06:27 -0400

    Update application.py

--- application.py
+++ application.py
@@ -106,7 +106,7 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Congratulations</h1>
+    <h1>Onochie Umeogu is learning how to be a committed and resourceful DEVOPS. Efe will be proud. Congratulations!</h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
     <p>This environment is launched with Elastic Beanstalk Python Platform</p>
   </div>
